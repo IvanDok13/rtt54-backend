a41c63e65852576999809e329a40b7dd0c85c163
Ivan Klochkov <vany-nov13@yandex.ru>
2025-12-05 13:42:43 -0800

    feat: update env

--- server.js
+++ server.js
@@ -6,21 +6,19 @@ const mongoose = require('mongoose');
 // Set up all variables in the .env file
 require('dotenv').config();
 
-
 // Database Connection
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log('Successfully connected to MongoDB!'))
-.catch(err => console.error('Connection error', err));
-
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log('Successfully connected to MongoDB!'))
+  .catch(err => console.error('Connection error', err));
 
 const PORT = process.env.PORT || 4000;
 const app = express();
 
-
 // ========= Middlewares =================
 app.use(morgan('dev')); // logger
 app.use(express.json()); // body parser
-app.use(cors({origin: "http://localhost:5173"}));
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 require('./config/passport');
 
 // ========= Routes ======================
@@ -29,9 +27,9 @@ app.use('/api/projects', require('./routes/projectRoutes'));
 
 // Use this route to setup the API documentation
 app.get('/', (req, res) => {
-    res.send('Welcome to my API!');
+  res.send('Welcome to my API!');
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port: ${PORT}`)
+  console.log(`Server running on port: ${PORT}`);
 });
