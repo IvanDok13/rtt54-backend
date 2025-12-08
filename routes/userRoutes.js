138a9e7ef0c69c29d330ddd2ddb15964b31cb399
Ivan Klochkov <vany-nov13@yandex.ru>
2025-12-08 11:46:09 -0800

    feat: update routes with new func

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require('express');
 const {
   getAllUsers,
   getUserById,
   registerUser,
   loginUser,
-} = require("../controllers/userController");
+} = require('../controllers/userController');
 
-const { authMiddleware,  adminOnly, signToken} = require("../middlewares/auth");
+const { authMiddleware, adminOnly, signToken } = require('../middlewares/auth');
 const passport = require('passport');
 
 // Router
@@ -15,23 +15,22 @@ const userRouter = express.Router();
 /**
  * GET /api/user/
  */
-userRouter.get("/", authMiddleware, adminOnly, getAllUsers);
+userRouter.get('/', authMiddleware, adminOnly, getAllUsers);
 
 /**
  * GET /api/user/:id
  */
-userRouter.get("/:id", getUserById);
+userRouter.get('/:id', getUserById);
 
 /**
  * POST /api/user/register
  */
-userRouter.post("/register", registerUser);
+userRouter.post('/register', registerUser);
 
 /**
  * POST /api/user/login
  */
-userRouter.post("/login", loginUser);
-
+userRouter.post('/login', loginUser);
 
 // Route to start the OAuth flow
 // When a user visits this URL, they will be redirected to GitHub to log in.
@@ -40,13 +39,12 @@ userRouter.get(
   passport.authenticate('github', { scope: ['user:email'] }) // Request email scope
 );
 
-
 // The callback route that GitHub will redirect to after the user approves.
 userRouter.get(
   '/auth/github/callback',
   passport.authenticate('github', {
     failureRedirect: '/login', // Where to redirect if user denies
-    session: false // We are using tokens, not sessions
+    session: false, // We are using tokens, not sessions
   }),
   (req, res) => {
     // At this point, `req.user` is the user profile returned from the verify callback.
