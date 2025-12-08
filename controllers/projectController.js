b8620ec8bfed263101d183faa5fb206044f985d4
Ivan Klochkov <vany-nov13@yandex.ru>
2025-12-08 11:43:38 -0800

    feat: move controllers - create, get, get by id for projects to separate file

---
+++ controllers/projectController.js
@@ -0,0 +1,53 @@
+const Project = require('../models/Project');
+
+const getAllProjects = async (req, res) => {
+  try {
+    const projects = await Project.find({ user: req.user._id });
+    res.json(projects);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const getProjectById = async (req, res) => {
+  try {
+    const { projectId } = req.params;
+    const project = await Project.findById(projectId);
+
+    if (!project) {
+      return res
+        .status(404)
+        .json({ message: `Project with id: ${projectId} not found!` });
+    }
+
+    // Authorization
+    console.log(req.user._id);
+    console.log(project.user);
+
+    if (project.user.toString() !== req.user._id) {
+      return res.status(403).json({ message: 'User is not authorized!' });
+    }
+
+    res.json(project);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const createProject = async (req, res) => {
+  try {
+    const newProject = await Project.create({
+      ...req.body,
+      user: req.user._id,
+    });
+
+    res.status(201).json(newProject);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = { getAllProjects, getProjectById, createProject };
